c04099bddd747159c382ebc30bf59a8349731467
JosueAvelar <avelarjosue1@gmail.com>
2022-04-22 22:00:57 -0600

    Archivos relacionados a Productos

---
+++ MedOne/app/src/main/java/sv/edu/udb/medone/AddProductos.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.medone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProductos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_productos);
+    }
+}
---
+++ MedOne/app/src/main/java/sv/edu/udb/medone/EditProductos.java
@@ -0,0 +1,14 @@
+package sv.edu.udb.medone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProductos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_productos);
+    }
+}
