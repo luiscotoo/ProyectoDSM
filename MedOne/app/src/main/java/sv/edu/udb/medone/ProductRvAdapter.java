4f6af4b75b5e1cb6ab18d186a32fe8eb1191bc96
Dayanna <dzuniga145@gmail.com>
2022-04-23 15:34:43 -0600

    cambios
    aiuda

--- MedOne/app/src/main/java/sv/edu/udb/medone/ProductRvAdapter.java
+++ MedOne/app/src/main/java/sv/edu/udb/medone/ProductRvAdapter.java
@@ -1,29 +1,79 @@
 package sv.edu.udb.medone;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ProductRvAdapter extends BaseAdapter {
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class ProductRvAdapter extends RecyclerView.Adapter<ProductRvAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<ProductRvModal> productRvModalArrayList;
+    private ProductClickInterface productClickInterface;
+    int lastPosition=-1;
+    public ProductRvAdapter(ArrayList<ProductRvModal> productRvModalArrayList,Context context, ProductClickInterface productClickInterface){
+        this.productRvModalArrayList=productRvModalArrayList;
+        this.productClickInterface=productClickInterface;
+        this.context= context;
+    }
+
+    @NonNull
     @Override
-    public int getCount() {
-        return 0;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.course_rv_item,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        ProductRvModal productRvModal= productRvModalArrayList.get(position);
+        holder.productTV.setText(productRvModal.getProductName());
+        holder.prductPriceTV.setText(productRvModal.getProductPrice());
+        Picasso.get().load(productRvModal.getProductImg()).into(holder.productIV);
+        setAnimation(holder.itemView,position);
+        holder.productIV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                productClickInterface.onProductClick(position);
+            }
+        });
+    }
+    private  void setAnimation(View view, int position){
+        if(position>lastPosition){
+            Animation animation= AnimationUtils.loadAnimation(context, android.R.anim.slide_in_left);
+            view.setAnimation(animation);
+            lastPosition=position;
+        }
+    }
+    @Override
+    public int getItemCount() {
+        return productRvModalArrayList.size();
     }
 
-    @Override
-    public long getItemId(int i) {
-        return 0;
-    }
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        private ImageView productIV;
+        private TextView productTV, prductPriceTV;
+       public ViewHolder(@NonNull View itemView) {
+           super(itemView);
+           productIV= itemView.findViewById(R.id.idIVCourse);
+           productTV=itemView.findViewById(R.id.idTVCOurseName);
+           prductPriceTV=itemView.findViewById(R.id.idTVCoursePrice);
+       }
+   }
+   public interface ProductClickInterface{void onProductClick(int position);}
 
-    @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
-    }
 }
--- MedOne/app/src/main/java/sv/edu/udb/medone/ProductosPrincipal.java
+++ MedOne/app/src/main/java/sv/edu/udb/medone/ProductosPrincipal.java
@@ -1,14 +1,179 @@
 package sv.edu.udb.medone;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
 
 public class ProductosPrincipal extends AppCompatActivity {
+    private FloatingActionButton addProductFAB;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    private RecyclerView productRV;
+    private FirebaseAuth mAuth;
+    private ProgressBar loadingPB;
+    private ArrayList<ProductRvModal> productRVModalArrayList;
+    private ProductRvAdapter productRVAdapter;
+    private RelativeLayout productRL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos_principal);
+        productRV=findViewById(R.id.idRVProductos);
+        productRL=findViewById(R.id.idRLProductos);
+        loadingPB = findViewById(R.id.idPBLoading);
+        addProductFAB=findViewById(R.id.idFABAddProductos);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        productRVModalArrayList=new ArrayList<>();
+
+        databaseReference = firebaseDatabase.getReference("Product");
+        productRVAdapter=new ProductRvAdapter(productRVModalArrayList,this,this::onProductClick);
+        productRV.setLayoutManager(new GridLayoutManager(this,2));
+        productRV.setAdapter(productRVAdapter);
+        getProduct();
+    }
+    private void getProduct(){
+        productRVModalArrayList.clear();
+        databaseReference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                loadingPB.setVisibility(View.GONE);
+                productRVModalArrayList.add(snapshot.getValue(ProductRvModal.class));
+                productRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                loadingPB.setVisibility(View.GONE);
+                productRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+                loadingPB.setVisibility(View.GONE);
+                productRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                loadingPB.setVisibility(View.GONE);
+                productRVAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    
+    public void onProductClick(int position){
+        displayBottomSheet(productRVModalArrayList.get(position));
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // adding a click listener for option selected on below line.
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.idLogOut:
+                // displaying a toast message on user logged out inside on click.
+                Toast.makeText(getApplicationContext(), "User Logged Out", Toast.LENGTH_LONG).show();
+                // on below line we are signing out our user.
+                mAuth.signOut();
+                // on below line we are opening our login activity.
+                Intent i = new Intent(ProductosPrincipal.this, Login.class);
+                startActivity(i);
+                this.finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // on below line we are inflating our menu
+        // file for displaying our menu options.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    private void displayBottomSheet(ProductRvModal modal) {
+        // on below line we are creating our bottom sheet dialog.
+        final BottomSheetDialog bottomSheetTeachersDialog = new BottomSheetDialog(this, R.style.BottomSheetDialogTheme);
+        // on below line we are inflating our layout file for our bottom sheet.
+        View layout = LayoutInflater.from(this).inflate(R.layout.bottom_sheet_service, productRL,false);
+        // setting content view for bottom sheet on below line.
+        bottomSheetTeachersDialog.setContentView(layout);
+        // on below line we are setting a cancelable
+        bottomSheetTeachersDialog.setCancelable(false);
+        bottomSheetTeachersDialog.setCanceledOnTouchOutside(true);
+        // calling a method to display our bottom sheet.
+        bottomSheetTeachersDialog.show();
+        // on below line we are creating variables for
+        // our text view and image view inside bottom sheet
+        // and initialing them with their ids.
+        TextView serviceNameTV = layout.findViewById(R.id.idTVServiceName);
+        TextView serviceDescTV = layout.findViewById(R.id.idTVServiceDesc);
+        TextView serviceForTV = layout.findViewById(R.id.idTVSuitedFor);
+        TextView serviceTV = layout.findViewById(R.id.idTVServicePrice);
+        ImageView serviceIV = layout.findViewById(R.id.idIVService);
+        // on below line we are setting data to different views on below line.
+        serviceNameTV.setText(modal.getProductName());
+        serviceDescTV.setText(modal.getProductDescription());
+        serviceForTV.setText("Restricciones " + modal.getProductRestrictions());
+        serviceTV.setText("$" + modal.getProductPrice());
+        Picasso.get().load(modal.getProductImg()).into(serviceIV);
+        Button viewBtn = layout.findViewById(R.id.idBtnVIewDetails);
+        Button editBtn = layout.findViewById(R.id.idBtnEditService);
+
+        // adding on click listener for our edit button.
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // on below line we are opening our EditCourseActivity on below line.
+                Intent i = new Intent(ProductosPrincipal.this, EditService.class);
+                // on below line we are passing our course modal
+                i.putExtra("product", modal);
+                startActivity(i);
+            }
+        });
+        // adding click listener for our view button on below line.
+        viewBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(modal.getProductLink()));
+                startActivity(i);
+            }
+        });
     }
 }
